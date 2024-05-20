607910186c007cf94f33abcbf2bff69220feb652
sunguh0904 <sunguh0904@gmail.com>
2024-05-20 09:42:05 +0900

    24 05 20

--- LMS_REST_API_TEST/src/main/java/com/ohigraffers/practice/post/controller/PostController.java
+++ LMS_REST_API_TEST/src/main/java/com/ohigraffers/practice/post/controller/PostController.java
@@ -1,14 +1,31 @@
 package com.ohigraffers.practice.post.controller;
 
+import com.ohigraffers.practice.post.dto.request.PostCreateRequest;
+import com.ohigraffers.practice.post.dto.request.PostUpdateRequest;
+import com.ohigraffers.practice.post.dto.response.PostResponse;
 import com.ohigraffers.practice.post.dto.response.ResponseMessage;
 import com.ohigraffers.practice.post.model.Post;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 /* Swagger 문서화 시 Grouping 작성 */
+@Tag(name = "Post 관련 api")
 @RestController
 @RequestMapping("/posts")
 public class PostController {
@@ -27,61 +44,88 @@ public class PostController {
     /* 1. 전체 포스트 조회 */
     /* Swagger 문서화 시 설명 어노테이션 작성 */
     /* RequestMapping 어노테이션 작성 */
+    @Operation(summary = "포스트 전체 조회")
+    @GetMapping("/find")
     public ResponseEntity<ResponseMessage> findAllPosts() {
-
         /* 응답 데이터 설정 */
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
         /* Post 타입은 PostResponse 타입으로 변환해서 반환 */
         /* hateoas 적용 */
-
+        List<EntityModel<PostResponse>> postEntityModel = posts.stream().map(
+                post -> EntityModel.of(PostResponse.from(post),
+                linkTo(methodOn(PostController.class).findAllPosts()).withRel("posts"),
+                linkTo(methodOn(PostController.class).findPostByCode(post.getCode())).withSelfRel()
+                )).toList();
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("posts", postEntityModel);
+        ResponseMessage resMsg = new ResponseMessage(200, "조회 성공", resMap);
         /* ResponseEntity 반환 */
-        return null;
+        return new ResponseEntity<>(resMsg, headers, HttpStatus.OK);
     }
 
     /* 2. 특정 코드로 포스트 조회 */
     /* Swagger 문서화 시 설명 어노테이션 작성 */
     /* RequestMapping 어노테이션 작성 */
-    public ResponseEntity<ResponseMessage> findPostByCode(/* 필요 매개변수 선언 */) {
-
+    @Operation(summary = "특정 코드로 포스트 조회")
+    @GetMapping("/find/{code}")
+    public ResponseEntity<ResponseMessage> findPostByCode(@PathVariable long code) {
         /* 응답 데이터 설정 */
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
         /* Post 타입은 PostResponse 타입으로 변환해서 반환 */
         /* hateoas 적용 */
-
+        Post foundPost = posts.stream().filter(post -> post.getCode() == code).toList().get(0);
+        PostResponse response = PostResponse.from(foundPost);
         /* ResponseEntity 반환 */
-        return null;
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("posts", response);
+        ResponseMessage resMsg = new ResponseMessage(200, "조회 성공", resMap);
+        return ResponseEntity.ok().headers(headers).body(resMsg);
     }
 
     /* 3. 신규 포스트 등록 */
     /* Swagger 문서화 시 설명 어노테이션 작성 */
     /* RequestMapping 어노테이션 작성 */
-   public ResponseEntity<Void> registPost(/* 필요 매개변수 선언, 유효성 검사 */) {
-
+    @Operation(summary = "신규 포스트 등록")
+    @PostMapping("/regist")
+   public ResponseEntity<Void> registPost(@RequestBody PostCreateRequest pcr) {
        /* 리스트에 추가 */
-
+        Long lastPost = posts.get(posts.size() - 1).getCode();
+       posts.add(new Post(lastPost + 1, pcr.getTitle(), pcr.getContent(), pcr.getWriter()));
        /* ResponseEntity 반환 */
-       return null;
+       return ResponseEntity.created(URI.create("/posts/regist" + posts.get(posts.size() - 1).getCode())).build();
    }
 
    /* 4. 포스트 제목과 내용 수정 */
    /* Swagger 문서화 시 설명 어노테이션 작성 */
    /* RequestMapping 어노테이션 작성 */
-    public ResponseEntity<Void> modifyPost(/* 필요 매개변수 선언, 유효성 검사 */) {
-
+    @Operation(summary = "포스트 제목 내용 수정")
+    @PutMapping("/update/{postCode}")
+    public ResponseEntity<Void> modifyPost(@PathVariable int postCode, @RequestBody PostUpdateRequest pur) {
         /* 리스트에서 찾아서 수정 */
         /* 수정 메소드 활용 */
-
+        Post foundPost = posts.stream().filter(post -> post.getCode() == postCode).toList().get(0);
+        foundPost.modifyTitleAndContent(pur.getTitle(), pur.getContent());
         /* ResponseEntity 반환 */
-        return null;
+        return ResponseEntity.created(URI.create("/posts/update/" + postCode)).build();
     }
 
     /* 5. 포스트 삭제 */
     /* Swagger 문서화 시 설명 어노테이션 작성 */
     /* RequestMapping 어노테이션 작성 */
-    public ResponseEntity<Void> removeUser(/* 필요 매개변수 선언 */) {
-
+    @Operation(description = "포스트 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "포스트 삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "포스트 삭제 실패")
+    })
+    @DeleteMapping("/posts/{postCode}")
+    public ResponseEntity<Void> removeUser(@PathVariable int postCode) {
         /* 리스트에서 찾아서 삭제 */
-
+        Post foundPost = posts.stream().filter(post -> post.getCode() == postCode).toList().get(0);
+        posts.remove(foundPost);
         /* ResponseEntity 반환 */
-        return null;
+        return ResponseEntity.noContent().build();
     }
 
 }
--- LMS_REST_API_TEST/src/main/java/com/ohigraffers/practice/post/dto/request/PostUpdateRequest.java
+++ LMS_REST_API_TEST/src/main/java/com/ohigraffers/practice/post/dto/request/PostUpdateRequest.java
@@ -1,19 +1,33 @@
 package com.ohigraffers.practice.post.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 /* Swagger 문서화 시 설명 어노테이션 작성 */
+@Schema(description = "포스트 수정")
 public class PostUpdateRequest {
 
     /* Swagger 문서화 시 설명 어노테이션 작성 */
     /* 필수 값이므로 유효성 검사 어노테이션 작성 */
+    @NotNull(message = "타이틀은 반드시 입력되어야 합니다.")
+    @NotBlank(message = "타이틀은 공백일 수 없습니다.")
+    @Size(max = 20, message = "타이틀은 길이 20을 초과할 수 없습니다.")
+    @Schema(description = "포스트 제목 수정")
     private String title;
 
     /* Swagger 문서화 시 설명 어노테이션 작성 */
     /* 필수 값이므로 유효성 검사 어노테이션 작성 */
+    @NotNull(message = "컨텐트는 반드시 입력되어야 합니다.")
+    @NotBlank(message = "컨텐트는 공백일 수 없습니다.")
+    @Length(max = 1000, message = "컨텐트는 길이 1000을 초과할 수 없습니다.")
+    @Schema(description = "포스트 내용 수정")
     private String content;
 
 }
